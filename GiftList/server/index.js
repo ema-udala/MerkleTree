d8c4c2fa8d856c401b7d69c56645856ff3ed154b
SPACEMAN <emmanueludala@gmail.com>
2023-09-04 11:53:10 +0100

    Add files via upload

---
+++ GiftList/server/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const verifyProof = require('../utils/verifyProof');
+const MerkleTree = require('../utils/MerkleTree');
+const niceList = require('../utils/niceList');
+
+const port = 1225;
+
+const app = express();
+app.use(express.json());
+
+// TODO: hardcode a merkle root here representing the whole nice list
+// paste the hex string in here, without the 0x prefix
+const MERKLE_ROOT = '';
+
+app.post('/gift', (req, res) => {
+  // grab the parameters from the front-end here
+  const { proof, name } = req.body;
+
+  // TODO: prove that a name is in the list 
+	const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+  if(isInTheList) {
+    res.send("You got a toy robot!");
+  }
+  else {
+    res.send("You are not on the list :(");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
